818b33c641b6cec2032f82ac2024fe49b8cb9797
chenhui777 <1523441772@qq.com>
2018-12-13 19:15:18 +0800

    sql第一版

---
+++ sql/haier.sql
@@ -0,0 +1,98 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2018-12-13 19:09:54
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "goods"
+#
+
+CREATE TABLE `goods` (
+  `goodsId` varchar(30) NOT NULL,
+  `goodsName` varchar(255) DEFAULT NULL,
+  `goodsType` varchar(30) DEFAULT NULL,
+  `goodsPrice` varchar(30) DEFAULT NULL,
+  `goodsCount` varchar(30) DEFAULT NULL,
+  `goodsDesc` varchar(30) DEFAULT NULL,
+  `goodsImg` varchar(30) DEFAULT NULL,
+  `beiyong1` varchar(30) DEFAULT NULL,
+  `beiyong2` varchar(30) DEFAULT NULL,
+  `beiyong3` varchar(30) DEFAULT NULL,
+  `beiyong4` varchar(30) DEFAULT NULL,
+  `beiyong5` varchar(30) DEFAULT NULL,
+  `beiyong6` varchar(30) DEFAULT NULL,
+  `beiyong7` varchar(30) DEFAULT NULL,
+  `beiyong8` varchar(30) DEFAULT NULL,
+  `beiyong9` varchar(30) DEFAULT NULL,
+  `beiyong10` varchar(30) DEFAULT NULL,
+  `beiyong11` varchar(30) DEFAULT NULL,
+  `beiyong12` varchar(30) DEFAULT NULL,
+  `beiyong13` varchar(30) DEFAULT NULL,
+  `beiyong14` varchar(255) DEFAULT NULL,
+  `beiyong15` varchar(255) DEFAULT NULL,
+  `beiyong16` varchar(255) DEFAULT NULL,
+  `beiyong17` varchar(255) DEFAULT NULL,
+  `beiyong18` varchar(255) DEFAULT NULL,
+  `beiyong19` varchar(255) DEFAULT NULL,
+  `beiyong20` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`goodsId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "goods"
+#
+
+INSERT INTO `goods` VALUES ('10001',NULL,NULL,'12.9',NULL,'轩涵 衣帽架 尾翼多色糖果衣架','../img/d1.jpg','立即查看详情','您现在的位置：首页> 居家生活> 储物家具> 挂衣架> 轩涵','轩涵 挂衣架 轩涵 晾衣架 宽肩小铁钩挂衣架','【12.12年终盛典】活动时间12月11日10点-16日24','无痕不起肩 加宽成人衣挂','8件','0','../img/jia1.jpg','98','5','66','陕西西安雁塔区丈八沟街道','../img/jia1.jpg','../img/jia2.jpg','../img/jia3.jpg','../img/jia4.jpg','../img/jia5.jpg','1',NULL,NULL),('10002',NULL,NULL,'59',NULL,'醛净界 家庭清洁 除醛净味布 5片1包醛净界','../img/d2.jpg','立即查看详情','您现在的位置：首页> 居家生活> 清洁用品> 家庭清洁> 醛','醛净界 家庭清洁 醛净界 家庭清洁 除醛净味布 5片/包','【清洁用品专场】活动时间12月7日10点-16日24点，活动','除醛除味 抑菌杀毒 防潮祛湿 可随意裁剪 孕妇可随身携带','263件','66','../img/quan1.jpg','19734','5','0','山东青岛崂山区中韩街道','../img/quan1.jpg','../img/quan2.jpg','../img/quan3.jpg','../img/quan4.jpg','../img/quan5.jpg','1',NULL,NULL),('10003',NULL,NULL,'259',NULL,'SWISSWIN 箱包 拉杆箱 SW8802 45*26*6','../img/d3.jpg','立即查看详情','您现在的位置：首页> 居家生活> 箱包手袋> 拉杆箱> SW','SWISSWIN 拉杆箱 SWISSWIN 箱包 拉杆箱 S',NULL,'美观 便捷 手感舒适 耐磨耐刮 引领时尚潮流','50件','4','../img/xiang1.jpg','98','5','4','陕西西安雁塔区丈八沟街道','../img/xiang1.jpg','../img/xiang2.jpg','../img/xiang3.jpg','../img/xiang4.jpg','../img/xiang5.jpg','1',NULL,NULL),('10004',NULL,NULL,'12.9',NULL,'轩涵 衣帽架 日式素色简约12夹衣架轩涵 衣帽架 日式素','../img/d4.jpg','立即查看详情','您现在的位置：首页> 居家生活> 服饰鞋类> 内衣> 竹舒康','竹舒康 内衣 竹舒康 内衣 舒适纯棉男女袜子','【12.12年终盛典】活动时间12月11日10点-16日24','优质纯棉 保暖舒适','77件','9','../img/wa1.jpg','9939','5','11','山东青岛崂山区中韩街道','../img/wa1.jpg','../img/wa2.jpg','../img/wa3.jpg','../img/wa4.jpg','../img/wa5.jpg','1',NULL,NULL),('10005',NULL,NULL,'14.9',NULL,'涤尚 衣物清洁 配方升级洗衣片衣物清洁 配方升级洗','../img/d5.jpg','立即查看详情','您现在的位置：首页> 居家生活> 清洁用品> 衣物清洁> 涤','涤尚 衣物清洁 涤尚 衣物清洁 配方升级洗衣片','【12.12年终盛典】活动时间12月11日10点-16日24','防止衣物串色 洗净力高出标准3倍 安全无毒 皮肤零刺激','1724件','146','../img/di1.jpg','914','5','146','陕西西安雁塔区丈八沟街道','../img/di1.jpg','../img/di2.jpg','../img/di3.jpg','../img/di4.jpg','../img/di5.jpg','1',NULL,NULL),('10006',NULL,NULL,'9.9',NULL,'竹舒康 毛巾浴巾 大方格毛巾 M-028 30*66CM','../img/d6.jpg','立即查看详情','您现在的位置：首页> 居家生活> 家纺> 乳胶枕> DISN','DISNEY 乳胶枕 DISNEY 枕头 迪士尼天然乳胶颗粒','【12.12年终盛典】活动时间12月11日10点-16日24','乳胶枕芯 呵护肩颈 舒芯入睡','0件','0','../img/zhen1.jpg',NULL,'5','0','山东青岛崂山区中韩街道','../img/zhen1.jpg','../img/zhen2.jpg','../img/zhen3.jpg','../img/zhen4.jpg','../img/zhen5.jpg','1',NULL,NULL),('10007',NULL,NULL,'9.9',NULL,'物语本色 纸品/湿巾 原生竹浆抽纸 6包迷你装','../img/d7.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10008',NULL,NULL,'49.9',NULL,'斑布 纸品/湿巾 竹纤维本色无芯卫生卷纸 30卷','../img/d8.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10009',NULL,NULL,'19.9',NULL,'钟情 纸品 原生本色竹浆抽纸16包 箱装','../img/d9.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10010',NULL,NULL,'49.9',NULL,'物语本色 纸品/湿巾 竹纤维本色卷纸 12卷/提 48卷/箱','../img/d10.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10011',NULL,NULL,'29.9',NULL,'物语本色 纸品/湿巾 顺逛精选 原生竹浆本色抽纸 18包/','../img/d11.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10012',NULL,NULL,'32.9',NULL,'物语本色 纸品/湿巾 顺逛精选 原生木浆超柔系列抽纸 18','../img/d12.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10013',NULL,NULL,'32',NULL,'新食器 水杯 不锈钢商务保温杯（红）','../img/d13.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10014',NULL,NULL,'36',NULL,'新食器 保温杯 学生时尚真空保温杯500ml（蓝色）','../img/d14.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10015',NULL,NULL,'178',NULL,'aichiw/爱奇屋 保温杯 旅行保温杯 AI-L52','../img/d15.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10016',NULL,NULL,'269',NULL,'爱仕达 锅具 金刚旋风新不粘炒锅 CL32T5TG 双十一特','../img/d16.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10017',NULL,NULL,'159',NULL,'新食器 锅具 深型煎炒锅 XSQ-SJG2801 双十一特卖','../img/d17.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10018',NULL,NULL,'79',NULL,'炊大皇 锅具 溢彩奶锅 WG14306 18cm','../img/d18.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10019',NULL,NULL,'32',NULL,'蓝月亮 衣物清洁 深层洁净护理洗衣液袋装 1kg*2（薰衣','../img/d19.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10020',NULL,NULL,'11.9',NULL,'康吉 家庭清洁 一次性清洁懒人抹布','../img/d20.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10021',NULL,NULL,'9.9',NULL,'小宝满 家庭清洁 加厚平口式垃圾袋 200只装','../img/d21.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10022',NULL,NULL,'29',NULL,'笠吉净 毛巾浴巾 小方巾套装 双十一特卖','../img/d22.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10023',NULL,NULL,'298',NULL,'泰神睡 乳胶枕 高低按摩枕 双十一特卖','../img/d23.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10024',NULL,NULL,'139',NULL,'LOVO 被子 LOVO家纺 柔梦四孔春秋被 冬被200*2','../img/d24.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10025',NULL,NULL,'12.5',NULL,'居家家 烘焙工具 纳米海绵擦 K5426 10片装','../img/d25.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10026',NULL,NULL,'24.5',NULL,'居家家 储物置物 多功能厨房沥水置物架 K6986','../img/d26.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),('10027',NULL,NULL,'26',NULL,'居家家 厨房用品 加厚防滑切菜板 P0403','../img/d27.jpg','立即查看详情',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+#
+# Structure for table "goodslist"
+#
+
+CREATE TABLE `goodslist` (
+  `goodsId` varchar(255) DEFAULT NULL,
+  `goodsUrl` varchar(30) NOT NULL,
+  `goodsTitle` varchar(30) NOT NULL,
+  `goodsPrice` varchar(30) NOT NULL,
+  `goodsDetail` varchar(30) NOT NULL,
+  UNIQUE KEY `goodsId` (`goodsId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "goodslist"
+#
+
+INSERT INTO `goodslist` VALUES ('1001','../img/depart1.jpg','轩涵 衣帽架 尾翼多色糖果衣架\r\n\r\n','￥12.9','立即查看详情'),('1002','../img/depart2.jpg','醛净界 家庭清洁 除醛净味布 5片/包\r\n\r\n\r\n\r\n','￥59','立即查看详情'),('1003','../img/depart2.jpg','醛净界 家庭清洁 除醛净味布 5片/包\r\n','￥59','立即查看详情'),('1004','../img/depart3.jpg','SWISSWIN 箱包 拉杆箱 SW8802 45*26*6','￥259','立即查看详情'),('1005','../img/depart4.jpg','轩涵 衣帽架 日式素色简约12夹衣架\r\n','￥12.9','立即查看详情'),('1006','../img/depart5.jpg','涤尚 衣物清洁 配方升级洗衣片\r\n','￥14.9','立即查看详情'),('1007','../img/depart6.jpg','竹舒康 毛巾浴巾 大方格毛巾 M-028 30*66CM\r\n','￥9.9','立即查看详情');
+
+#
+# Structure for table "login"
+#
+
+CREATE TABLE `login` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "login"
+#
+
+INSERT INTO `login` VALUES ('huawei','45678945'),('xiaomi','45678945'),('aaa111','aaa111'),('aaa111','aaa111'),('aaa1111','aaa1111'),('zhangsan777','zhangsan777');
+
+#
+# Structure for table "shoppingcenter"
+#
+
+CREATE TABLE `shoppingcenter` (
+  `goodsId` varchar(30) NOT NULL,
+  `vipName` varchar(30) NOT NULL,
+  `goodsCount` varchar(30) NOT NULL,
+  `goodsPrice` varchar(30) NOT NULL,
+  `goodsImg` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "shoppingcenter"
+#
+
+INSERT INTO `shoppingcenter` VALUES ('10002','醛净界 家庭清洁 醛净界 家庭清洁 除醛净味布 5片/包','5','59','../img/quan2.jpg'),('10006','DISNEY 乳胶枕 DISNEY 枕头 迪士尼天然乳胶颗粒','1','9.9','../img/zhen2.jpg');
